fbfc21a84964c4eb6bcd6800e06a61e02e0980c1
Renārs Masaļskis <74372830+rek1ngs@users.noreply.github.com>
2025-04-03 20:31:48 +0000

    03.04.25

--- Main.py
+++ Main.py
@@ -3,7 +3,8 @@ import asyncio
 import logging
 import sys
 from os import getenv
-import os
+from aiogram import Bot, Dispatcher, types
+import logging
 
 # from aiogram import Bot, Dispatcher, html
 # from aiogram.client.default import DefaultBotProperties
@@ -12,7 +13,23 @@ import os
 # from aiogram.types import Message
 load_dotenv(find_dotenv())
 
-TOKEN = os.getenv('TOKEN')
+TOKEN = getenv('TOKEN')
 
-#dp = Dispatcher()
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
+@dp.message()
+async def echo(message: types.Message):
+    await bot.send_message(
+        chat_id = message.chat.id,
+        text= "wait"
+    )
+    await message.answer(text=message.text)
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
