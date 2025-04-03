3fe691bb582c4235926097c9a3e9b5b1d85eea59
Renārs Masaļskis <74372830+rek1ngs@users.noreply.github.com>
2025-04-03 16:53:44 +0000

    03.04.25 (GitIgnere)

--- Main.py
+++ Main.py
@@ -1,15 +1,18 @@
+from dotenv import load_dotenv, find_dotenv
 import asyncio
 import logging
 import sys
 from os import getenv
+import os
 
-from aiogram import Bot, Dispatcher, html
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+# from aiogram import Bot, Dispatcher, html
+# from aiogram.client.default import DefaultBotProperties
+# from aiogram.enums import ParseMode
+# from aiogram.filters import CommandStart
+# from aiogram.types import Message
+load_dotenv(find_dotenv())
 
-TOKEN = getenv("BOT_TOKEN")
+TOKEN = os.getenv('TOKEN')
 
-dp = Dispatcher()
+#dp = Dispatcher()
 
