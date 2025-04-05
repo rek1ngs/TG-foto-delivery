03fed0333b5e3df50ffc8d6e1aa01d8754107253
Renārs Masaļskis <74372830+rek1ngs@users.noreply.github.com>
2025-04-05 21:45:19 +0000

    06.04.25

--- Main.py
+++ Main.py
@@ -3,22 +3,19 @@ import asyncio
 import logging
 import sys
 from os import getenv
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, Router, F
 import logging
+from aiogram.types import ContentType
 
-# from aiogram import Bot, Dispatcher, html
-# from aiogram.client.default import DefaultBotProperties
-# from aiogram.enums import ParseMode
-# from aiogram.filters import CommandStart
-# from aiogram.types import Message
 load_dotenv(find_dotenv())
 
 TOKEN = getenv('TOKEN')
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+rt = Router()
 
-@dp.message()
+@dp.message(F.text)
 async def echo(message: types.Message):
     await bot.send_message(
         chat_id = message.chat.id,
@@ -26,8 +23,13 @@ async def echo(message: types.Message):
     )
     await message.answer(text=message.text)
 
+@rt.message(F.photo)
+async def photo(message: types.Message):
+    await message.answer(text = "This is foto")
+
 async def main():
     logging.basicConfig(level=logging.INFO)
+    dp.include_router(rt)
     await dp.start_polling(bot)
 
 
