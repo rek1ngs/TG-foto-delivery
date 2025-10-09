267b1907502d50a45f233007f5aaa6f287bfe749
rek1ngs <renar.m34@outlook.com>
2025-10-10 02:09:47 +0300

    font

--- Marker.py
+++ Marker.py
@@ -11,7 +11,7 @@ def watermark(image_bytes: BytesIO):
     x, y = int(w / 2), int(h / 2)
 
     try:
-        font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/Arial.ttf", 100)
+        font = ImageFont.truetype("DejaVuSans.ttf", font_size)
     except OSError:
         font = ImageFont.load_default()
 
